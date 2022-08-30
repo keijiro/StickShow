9bc76fdf5a597af379865a88ead3dc316d3b82f0
Keijiro Takahashi <keijiro@gmail.com>
2022-08-30 17:35:19 +0900

    Refactoring

--- Assets/Scripts/Legion.cs
+++ Assets/Scripts/Legion.cs
@@ -3,6 +3,7 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
 
 [System.Serializable]
 struct Legion
@@ -57,29 +58,53 @@ struct Legion
 
     #region Stick animation
 
-    public float4x4 GetStickMatrix(float2 pos, float time)
+    public float4x4 GetStickMatrix(float2 pos, float time, uint seed)
     {
+        var rand = new Random(seed);
+        rand.NextUInt4();
+
+        // Cyclic animation phase parameter
         var phase = 2 * math.PI * swingFrequency * time;
-        var ntime = time * 0.234f;
-        var nvalue1 = noise.snoise(math.float3(pos.x, ntime, pos.y));
-        var nvalue2 = noise.snoise(math.float3(pos.x, ntime + 100, pos.y));
-        var angle = math.cos(phase + nvalue1);
-        var origin = math.float3(pos.x, 0, pos.y);
-        var axis = math.normalize(math.float3(nvalue2, 0, 1));
+        phase += noise.snoise(math.float3(pos, time * 0.57f));
+
+        // Animation origin (shoulder position)
+        var origin = float3.zero;
+        origin.xz = pos + rand.NextFloat2(-0.3f, 0.3f) * seatInterval;
+        origin.y = rand.NextFloat(-0.2f, 0.2f);
+
+        // Swing angle
+        var angle = math.cos(phase);
+        var angle_unsmooth = math.smoothstep(-1, 1, angle) * 2 - 1;
+        angle = math.lerp(angle, angle_unsmooth, rand.NextFloat());
+        angle *= rand.NextFloat(0.3f, 1.0f);
+
+        // Swing axis
+        var dx = noise.snoise(math.float3(pos.yx, time * 0.23f + 100));
+        var axis = math.normalize(math.float3(dx, 0, 1));
+
+        // Stick offset (arm length)
+        var offset = swingOffset * rand.NextFloat(0.75f, 1.25f);
+
+        // Matrix composition
         var m1 = float4x4.Translate(origin);
         var m2 = float4x4.AxisAngle(axis, angle);
-        var m3 = float4x4.Translate(math.float3(0, swingOffset, 0));
+        var m3 = float4x4.Translate(math.float3(0, offset, 0));
         return math.mul(math.mul(m1, m2), m3);
     }
 
-    public Color GetStickColor(float2 pos, float time)
+    public Color GetStickColor(float2 pos, float time, uint seed)
     {
-        var rnd = math.dot(pos, math.float2(23.13f, 134.78f));
-        rnd = math.frac(math.sin(rnd) * 44.583f);
-        var hue = math.frac(rnd + time * 0.83f);
-        var br = math.distance(pos, math.float2(0, -16));
-        br = math.sin(br * 0.73f - time * 2.8f) * 0.5f + 0.5f;
-        br = br * br * 50 + 0.1f;
+        var rand = new Random(seed);
+        rand.NextUInt4();
+
+        // Wave animation
+        var wave = math.distance(pos, math.float2(0, -16));
+        wave = math.sin(wave * 0.53f - time * 2.8f) * 0.5f + 0.5f;
+
+        // Hue / brightness
+        var hue = math.frac(rand.NextFloat() + time * 0.83f);
+        var br = wave * wave * 50 + 0.1f;
+
         return Color.HSVToRGB(hue, 1, br);
     }
 
@@ -97,12 +122,12 @@ struct LegionJob : IJobParallelFor
     public NativeSlice<Matrix4x4> matrices;
     public NativeSlice<Color> colors;
 
-    // Job bridge
     public void Execute(int i)
     {
         var (squad, seat) = config.GetCoordinatesFromIndex(i);
         var pos = config.GetPositionOnPlane(squad, seat);
-        matrices[i] = config.GetStickMatrix(pos, time);
-        colors[i] = config.GetStickColor(pos, time);
+        var seed = (uint)i * 2u + 123u;
+        matrices[i] = config.GetStickMatrix(pos, time, seed++);
+        colors[i] = config.GetStickColor(pos, time, seed++);
     }
 }
